abd4599c42d5228634fab50e75b9ea999933ff5b
imswebra <eric@imswebra.com>
2021-07-18 09:21:45 -0600

    Add common config file, update get-zones to use it

--- setup/cloudflare-get-zones.sh
+++ setup/cloudflare-get-zones.sh
@@ -2,10 +2,9 @@
 
 # Returns JSON list of zones matching the domain name, grab the ID from the valid entry
 
-# Variables:
-#   CLOUDFLARE_API_TOKEN   - Cloudflare API token from Cloudflare dashboard (Authentication tab)
-#   CLOUDFLARE_DOMAIN_NAME - Domain name e.g. github.com
+source ./config.txt
 
+echo "Getting Zone ID for domain from Cloudflare API..."
 if ! response=$(curl --fail-with-body -s \
     -X GET "https://api.cloudflare.com/client/v4/zones?name=$CLOUDFLARE_DOMAIN_NAME" \
     -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" -H "Content-Type:application/json");
@@ -15,4 +14,7 @@ then
     exit 1
 fi
 
-echo "Got Zone ID: $(echo $response | grep -Po '"id":"\K.*?(?=")' | head -1)"
+id=$(echo $response | grep -Po '"id":"\K.*?(?=")' | head -1)
+echo "Got Zone ID: $id"
+echo "Writing to config.txt"
+sed -i "s/\(^CLOUDFLARE_ZONE_ID=\)\(.*\)/\1$id/" ./config.txt
